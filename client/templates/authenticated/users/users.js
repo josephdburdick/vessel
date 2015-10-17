2a06631156ba740f295af82d21eda583f42a547a
Joe Burdick <josephdburdick@gmail.com>
2015-10-17 19:30:05 -0400

    Restructured directory for authenticated index/users templates.

--- client/templates/authenticated/users/users.js
+++ client/templates/authenticated/users/users.js
@@ -27,22 +27,25 @@ Template.users.events({
   'change [name="userRole"]': (event, template) => {
     let role = $(event.target).find('option:selected').val();
 
-    Meteor.call("setRoleOnUser", {
+    Meteor.call('setRoleOnUser', {
       user: event.target.getAttribute('data-userid'),
       role: role
     }, (error, response) => {
       if (error) {
-        Bert.alert(error.reason, "warning");
+        Bert.alert(error.reason, 'warning');
+      }
+      if (!error) {
+        Bert.alert(`Success! User role changed to ${Modules.client.toTitleCase(role)}.`, 'success');
       }
     });
   },
   'click .revoke-invite': ( event, template ) => {
-    if (confirm("Are you sure? This is permanent.")) {
-      Meteor.call("revokeInvitation", this._id, function(error, response) {
+    if (confirm('Are you sure? This is permanent.')) {
+      Meteor.call('revokeInvitation', this._id, function(error, response) {
         if (error) {
-          Bert.alert(error.reason, "warning");
+          Bert.alert(error.reason, 'warning');
         } else {
-          Bert.alert("Invitation revoked!", "success");
+          Bert.alert('Invitation revoked!', 'success');
         }
       });
     }
