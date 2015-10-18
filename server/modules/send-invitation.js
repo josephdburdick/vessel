c3681b0824d0fde610921f8625fd5452ef7be65b
Joe Burdick <josephdburdick@gmail.com>
2015-10-17 20:40:18 -0400

    Server side render email template.

--- server/modules/send-invitation.js
+++ server/modules/send-invitation.js
@@ -12,10 +12,19 @@ let _insertInvitation = ( invite ) => {
 let _prepareEmail = ( token ) => {
   let domain = Meteor.settings.private.domain;
   let url    = `http://${ domain }/invite/${ token }`;
+
+  SSR.compileTemplate( 'invitation', Assets.getText( 'email/templates/invitation.html' ) );
+  let html = SSR.render( 'invitation', { url: url } );
+  return html;
 };
 
 let _sendInvitation = ( email, content ) => {
-
+  Email.send({
+    to: email,
+    from: `${Meteor.settings.private.email.name} <${Meteor.settings.private.email.address}>`,
+    subject: `You're invited to ${Meteor.settings.private.brand.name}`,
+    html: content
+  });
 };
 
 Modules.server.sendInvitation = invitation;
