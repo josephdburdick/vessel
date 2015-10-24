a84050cb17b0bc8550d2f217b3c7a3b3aec0c952
Joe Burdick <josephdburdick@gmail.com>
2015-10-24 15:28:40 -0400

    Add more template helpers.

--- client/helpers/template.js
+++ client/helpers/template.js
@@ -1,3 +1,6 @@
+/*
+ * User instance
+ */
 Template.registerHelper( 'isCurrentUser', ( currentUser ) => {
   return currentUser === Meteor.userId() ? true : false;
 });
@@ -8,12 +11,52 @@ Template.registerHelper( 'disableIfAdmin', ( userId ) => {
   }
 });
 
+/*
+ * Comparison
+ */
+// http://themeteorchef.com/snippets/using-global-template-helpers/#tmc-writing-a-logic-helper
+Template.registerHelper( 'equals', ( a1, a2 ) => {
+ return a1 === a2;
+});
+Template.registerHelper( 'notEquals', ( a1, a2 ) => {
+  return a1 !== a2;
+});
+
+// http://themeteorchef.com/snippets/using-global-template-helpers/#tmc-css-classes
+Template.registerHelper( 'addClass', ( c1, className ) => {
+  return c1 ? className : '';
+});
+
+// Base
 Template.registerHelper( 'selected', ( v1, v2 ) => {
   return v1 === v2 ? true : false;
 });
 
+
+/*
+ * Time and date
+ */
 Template.registerHelper( 'humanDate', ( timestamp ) => {
   if ( timestamp ) {
     return moment( timestamp ).format( "MMMM Do, YYYY" );
+  }
+});
+
+// http://themeteorchef.com/snippets/using-global-template-helpers/#tmc-converting-an-iso-8601-string
+Template.registerHelper('ISOToHuman', ( isoString ) => {
+  if ( isoString ) {
+    return moment( isoString ).format( 'MMMM Do, YYYY' );
+  }
+});
+
+// http://themeteorchef.com/snippets/using-global-template-helpers/#tmc-converting-an-epoch-value
+Template.registerHelper('epochToHuman', ( timestamp ) => {
+  if ( timestamp ) {
+    let length = timestamp.toString().length;
+    if ( length === 10 ) {
+      return moment.unix( timestamp ).format( 'MMMM Do, YYYY' );
+    } else {
+      return moment.unix( timestamp / 1000 ).format( 'MMMM Do, YYYY' );
+    }
   }
 });
